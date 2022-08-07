9d4ddd4fecc5989edcfa2ec5cf58f6c413f2fbad
yuler <is.yuler@gmail.com>
2022-08-07 16:33:29 +0800

    build: adjust slides folder & bump deps

--- scripts/build.js
+++ scripts/build.js
@@ -3,29 +3,25 @@ import fs from 'node:fs'
 import {fileURLToPath} from 'node:url'
 import {execa} from 'execa'
 
-const target = 'slides'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const root = path.resolve(__dirname, '..')
-const folder = path.resolve(root, target)
+const folder = path.resolve(root, 'slides')
 const dist = path.resolve(root, 'dist')
 const slides = fs.readdirSync(folder)
 
+fs.mkdirSync(dist, {recursive: true})
+
 // Build slides
 for (const slide of slides) {
-	const entry = path.resolve(folder, slide, 'slides.md')
-	// TODO: Concurrent build
-	try {
-		await execa('slidev', [
-			'build',
-			entry,
-			'--out',
-			path.resolve(dist, slide),
-			'--base',
-			`/${slide}/`,
-		])
-	} catch (error) {
-		process.stderr.write(error.stderr)
-	}
+	const entry = path.resolve(folder, slide)
+	execa('slidev', [
+		'build',
+		entry,
+		'--out',
+		path.resolve(dist, slide),
+		'--base',
+		`/${slide}/`,
+	]).stdout.pipe(process.stdout)
 }
 
 // Write `index.html` to dist
